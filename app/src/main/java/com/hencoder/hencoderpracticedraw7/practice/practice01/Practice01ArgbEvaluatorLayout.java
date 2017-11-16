8b6cf7e4acff3489465c423199d7ef40e6676065
Administrator <1398565099@qq.com>
2017-11-16 18:29:15 +0800

    code update

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
@@ -38,7 +38,8 @@ public class Practice01ArgbEvaluatorLayout extends RelativeLayout {
             public void onClick(View v) {
                 ObjectAnimator animator = ObjectAnimator.ofInt(view, "color", 0xffff0000, 0xff00ff00);
                 // 在这里使用 ObjectAnimator.setEvaluator() 来设置 ArgbEvaluator，修复闪烁问题
-                animator.setInterpolator(new LinearInterpolator());
+//                animator.setEvaluator(new ArgbEvaluator());
+                animator.setInterpolator(new LinearInterpolator());//匀速变化
                 animator.setDuration(2000);
                 animator.start();
             }
