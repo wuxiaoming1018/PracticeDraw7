35cc36aeaf555089b147c67a36949d6005d3f198
Administrator <1398565099@qq.com>
2017-11-22 10:40:25 +0800

    code update

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/Practice05AnimatorSetLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/Practice05AnimatorSetLayout.java
@@ -47,7 +47,10 @@ public class Practice05AnimatorSetLayout extends RelativeLayout {
                 // 用 AnimatorSet 的方法来让三个动画协作执行
                 // 要求 1： animator1 先执行，animator2 在 animator1 完成后立即开始
                 // 要求 2： animator2 和 animator3 同时开始
-
+//                animatorSet.playSequentially(animator1,animator2,animator3);//从前到后依次播放
+//                animatorSet.playTogether(animator1,animator2,animator3);//一起播放
+                animatorSet.play(animator1).before(animator2);//先执行animator1,再执行animator2
+                animatorSet.playTogether(animator2,animator3);//一起执行
                 animatorSet.start();
             }
         });
